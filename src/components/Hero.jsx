9797926f7a726a2ed97181910c880548a1a757ea
uliadib12 <uliadib@gmail.com>
2024-01-10 00:20:25 +0700

    init

---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className="bg-gray-200 text-white flex justify-center items-center h-screen">
+        <h1 className="text-9xl uppercase font-black">Hero</h1>        
+    </div>
+  );
+}
