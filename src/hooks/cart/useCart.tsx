5af5e53e4e734a3bfe65b8e891b957b803ec4af8
uliadib12 <uliadib@gmail.com>
2024-01-22 16:38:17 +0700

    edit cart

--- src/hooks/cart/useCart.tsx
+++ src/hooks/cart/useCart.tsx
@@ -3,7 +3,8 @@ import CartModel from "../../models/cart/cartModel";
 import CartServices from "../../services/cart/cartServices";
 
 export default function useCart() : 
-    [CartModel, (products: CartModel) => void]{
+    [CartModel, (products: CartModel) => void]
+{
     const [products, setProducts] = useState<CartModel>(CartModel.EmptyCart());
 
     useEffect(() => {
