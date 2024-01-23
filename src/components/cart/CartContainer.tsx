cd6c3f0fc54592618d8bec90c1ca6d339fc34ce5
uliadib12 <uliadib@gmail.com>
2024-01-23 13:53:13 +0700

    add checkout

--- src/components/cart/CartContainer.tsx
+++ src/components/cart/CartContainer.tsx
@@ -31,10 +31,10 @@ export default function CartContainer() {
                     }
                 </div>
                 <div className="fixed bottom-0 left-0 right-0 p-4 flex justify-end z-50">
-                    <button className="flex gap-2 bg-pink-400 border-solid border-white border-2 px-4 py-2 rounded-md text-white">
+                    <a href="/checkout" className="flex gap-2 bg-pink-400 border-solid border-white border-2 px-4 py-2 rounded-md text-white">
                         Go To Checkout
                         <img src="/icons/bag-check.svg" width={20} height={20} alt="home" style={{filter: 'invert(1)'}}/>
-                    </button>
+                    </a>
                 </div>
             </div>
         </CartContext.Provider>
---
+++ src/components/checkout/OrderDetail.tsx
@@ -0,0 +1,7 @@
+export default function OrderDetail() {
+    return (
+        <div>
+            <h1>Order Detail</h1>
+        </div>
+    )
+}
