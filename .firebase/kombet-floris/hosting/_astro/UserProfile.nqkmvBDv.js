87b8c17829b6055cb211fb34f95d5e8600078b62
uliadib12 <uliadib@gmail.com>
2024-01-30 00:54:08 +0700

    add category fetch

---
+++ .firebase/kombet-floris/hosting/_astro/UserProfile.nqkmvBDv.js
@@ -0,0 +1,1 @@
+import{j as e}from"./jsx-runtime.G4i-1Giy.js";import"./index.UCAuQZPX.js";function r(){return e.jsx("div",{className:"flex flex-col items-center justify-center h-full",children:e.jsx("h1",{className:"text-3xl font-bold",children:"User@gmail.com"})})}export{r as default};
--- src/hooks/category/useCategory.tsx
+++ src/hooks/category/useCategory.tsx
@@ -4,71 +4,61 @@ import CategoryModel from "../../models/category/categoryModel";
 export default function useCategory() : CategoryModel {
     const [products, setProducts] = useState<CategoryModel>(CategoryModel.EmptyCategory());
 
+    const fetchProducts = async () => {
+        const response = await fetch(`${import.meta.env.PUBLIC_BACKEND}/api/v1/category/${getSearchParam()}`);
+        const data = await response.json();
+        
+        if (data.length === 0) {
+            return CategoryModel.EmptyCategory();
+        }
+
+        return parseData(data);
+    }
+
     useEffect(() => {
-        setProducts(
-            new CategoryModel([
-                 {
-                     id: 1,
-                     image: 'https://picsum.photos/seed/1/500/500',
-                     name: 'Perindo',
-                     minPrice: 1000000,
-                     maxPrice: 10000000,
-                 },
-                 {
-                     id: 2,
-                     image: 'https://picsum.photos/seed/2/500/500',
-                     name: 'Perindo',
-                     minPrice: 1000000,
-                     maxPrice: 10000000,
-                 },
-                 {
-                     id: 3,
-                     image: 'https://picsum.photos/seed/3/500/500',
-                     name: 'Perindo',
-                     minPrice: 1000000,
-                     maxPrice: 10000000,
-                 },
-                 {
-                     id: 4,
-                     image: 'https://picsum.photos/seed/4/500/500',
-                     name: 'Perindo',
-                     minPrice: 1000000,
-                     maxPrice: 10000000,
-                 },
-                 {
-                     id: 5,
-                     image: 'https://picsum.photos/seed/5/500/500',
-                     name: 'Perindo',
-                     minPrice: 1000000,
-                     maxPrice: 10000000,
-                 },
-                 {
-                     id: 6,
-                     image: 'https://picsum.photos/seed/6/500/500',
-                     name: 'Perindo',
-                     minPrice: 1000000,
-                     maxPrice: 1000000,
-                 },
-                 {
-                     id: 7,
-                     image: 'https://picsum.photos/seed/7/500/500',
-                     name: 'Perindo',
-                     minPrice: 1000000,
-                     maxPrice: 1000000,
-                 },
-                 {
-                     id: 8,
-                     image: 'https://picsum.photos/seed/8/500/500',
-                     name: 'Perindo',
-                     minPrice: 1000000,
-                     maxPrice: 1000000,
-                 },
-            ])
-        );
+        fetchProducts().then((data) => {
+            setProducts(data);
+        });
+
         return () => {
             setProducts(CategoryModel.EmptyCategory());
         }
     }, []);
 
     return products;
+}
+
+function getSearchParam() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('name');
+}
+
+function parseData(data: any) {
+    const products = data.map((product: any) => {
+        return {
+            id: product.id,
+            image: product.images[0],
+            name: product.name,
+            minPrice: getMinMaxPrice(product.variants).minPrice,
+            maxPrice: getMinMaxPrice(product.variants).maxPrice,
+        }
+    });
+    return new CategoryModel(products);
+}
+
+function getMinMaxPrice(variants: any) {
+    let minPrice = 0;
+    let maxPrice = 0;
+    variants.forEach((variant: any) => {
+        if (variant.price < minPrice) {
+            minPrice = variant.price;
+        }
+        if (variant.price > maxPrice) {
+            maxPrice = variant.price;
+        }
+        if (minPrice === 0) {
+            minPrice = maxPrice;
+        }
+    });
+    return { minPrice, maxPrice };
 }
